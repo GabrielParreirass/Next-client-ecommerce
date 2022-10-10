809c167d5049277a76a042a9672513c5e7d3cc1f
Gabriel Parreiras <gparreiras46@gmail.com>
2022-10-09 22:29:48 -0300

    first commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "../styles/Header.module.css";
 import Link from "next/link";
-import { motion } from "framer-motion";
+
 
 function Header() {
   return (
