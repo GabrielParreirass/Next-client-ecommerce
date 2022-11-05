948264eb4d028e2b5a4ac6794b89e6e217f17031
Gabriel Parreiras <gparreiras46@gmail.com>
2022-11-05 16:06:09 -0300

    footer upgrade

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import styles from '../styles/Footer.module.css'
+import { GrInstagram } from 'react-icons/gr';
+import Link from 'next/link';
+
 
 function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.footerItem}>Copyright © 2022 Diamond Store - Todos os direitos reservados</div>
+      <div className={styles.wrapperFooter}>
+        <ul>
+          <li>Politica de Privacidade</li>
+          <li>Envio e Entrega</li>
+        </ul>
+        <div className={styles.insta}>
+          <Link href="https://www.instagram.com/diamond.storebh/" rel='next' target="_blank"><GrInstagram/></Link>
+        </div>
+      </div>
     </div>
   )
 }
